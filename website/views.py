d5edb31d226cce377e2c4f2278632ce6b59c7948
Cameron Snow <cameronsnow@Camerons-MacBook-Pro.local>
2024-08-25 16:55:18 -0500

    First commit

---
+++ website/views.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+        
+            
+    return render_template("home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data.decode('utf-8'))
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return jsonify({})
+
+
+@views.route('/portfolio')
+def portfolio():
+    projects = [
+        {
+            "title": "Note Taking App",
+            "description": "A web application for creating and managing personal notes.",
+            "technologies": ["Python", "Flask", "SQLAlchemy", "HTML", "CSS", "JavaScript"],
+            "link": "https://github.com/cameronjsnow"
+        },
+        # Add more project dictionaries here
+    ]
+    return render_template("portfolio.html", user=current_user, projects=projects)
